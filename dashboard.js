58a89b86d46c513d36f7aab257094b512aa8e9db
GetOnForm <getonform@outlook.com>
2025-04-06 22:15:17 +0200

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,11 @@
 // dashboard.js
 
-const supabase = supabase.createClient(
+cconst { createClient } = supabase;
+const supabaseClient = createClient(
   'https://feyupwxdyriniiffghkc.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleXVwd3hkeXJpbmlpZmZnaGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5NjkwNzUsImV4cCI6MjA1OTU0NTA3NX0.VP6u6mmF9SCx1U6IuQegH-fBA4XSVRGwDQypjjf6Z1A'
 );
+
 
 const dummyUserId = '00000000-0000-0000-0000-000000000001';
 
